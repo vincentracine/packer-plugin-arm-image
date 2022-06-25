3f8a1c7eee355e7d8f79d7bb72248d779c96ba9f
vincentracine <vincentracine@hotmail.co.uk>
2022-06-25 13:37:32 +0100

    add logging

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
 	"github.com/solo-io/packer-plugin-arm-image/pkg/builder"
 	"github.com/solo-io/packer-plugin-arm-image/pkg/postprocessor"
 	"github.com/solo-io/packer-plugin-arm-image/version"
+	"os"
 )
 
 func main() {
@@ -12,6 +14,7 @@ func main() {
 	pps.RegisterBuilder(plugin.DEFAULT_NAME, builder.NewBuilder())
 	pps.RegisterPostProcessor(plugin.DEFAULT_NAME, postprocessor.NewFlasher())
 	pps.SetVersion(version.PluginVersion)
+	fmt.Println(os.Args[1:])
 	err := pps.Run()
 	if err != nil {
 		panic(err)
